241be8047d6e9fd663c4f352dc39fc6829ce028f
h-custodio <hprince0814@gmail.com>
2026-09-02 22:40:13 -0700

    import corrections and authentication check

--- app/plan-page/page.tsx
+++ app/plan-page/page.tsx
@@ -1,6 +1,25 @@
 import { PlanForm } from "@/components/plan/PlanForm";
+import { redirect } from "next/navigation";
+import supabaseServer from "@/lib/supabase/server";
 
-export default function PlanPage() {
+export default async function PlanPage() {
+    // verify that user is authenticated
+    const { data: {user}, error } = await supabaseServer.auth.getUser();
+
+
+    if (error) {
+        console.error("Failed to get user:", error);
+        return;
+    }
+
+
+    // No authenticated user
+    if (!user) {
+        console.log("user not authenticated, no access");
+        redirect("auth/login");
+    }
+
+
   return (
     <div>
       <PlanForm />
--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,6 +1,6 @@
 import { createBrowserClient } from "@supabase/ssr";
 import { SupabaseClient } from "@supabase/supabase-js";
-import type { Database } from "./database.types";
+import type { Database } from "@/types/database.types";
 
 // create/return Supabase client for code to communicate with
 export function createClient(): SupabaseClient {
