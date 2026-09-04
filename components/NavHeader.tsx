4b3162afbc30912dab765a7d67d5baf2b20ec2c7
Frank Mai <frankmai@PowerBook-100.local>
2026-09-03 18:42:16 -0700

    Added logic for display buttons for logged in users and removed the plan nav

--- components/NavHeader.tsx
+++ components/NavHeader.tsx
@@ -1,33 +1,48 @@
 import Link from "next/link";
+import LogoutButton from "@/components/LogoutButton";
 import { buttonVariants } from "@/components/ui/button";
 
 const NavHeader = () => {
+  // verify that user is authenticated
+  //   const {
+  //     data: { user },
+  //     error,
+  //   } = await supabase.auth.getUser();
+
+  //   if (error) {
+  //     console.error("Failed to get user:", error);
+  //     return;
+  //   }
+
+  const user = "";
+
   return (
     <header className="nav-header">
       <div className="flex justify-between">
-        <Link href="/" className="text-2xl font-bold">
+        <Link href="/" className="text-3xl font-bold">
           ourtinerary
         </Link>
 
         <nav className="flex gap-1">
-          <Link
-            href="/"
-            className={buttonVariants({ size: "lg", variant: "secondary" })}
-          >
-            Plans
-          </Link>
-          <Link
-            href="/dashboard"
-            className={buttonVariants({ size: "lg", variant: "secondary" })}
-          >
-            Account
-          </Link>
-          <Link
-            href="/auth/login"
-            className={buttonVariants({ size: "lg", variant: "default" })}
-          >
-            Sign in
-          </Link>
+          {user !== null && (
+            <div className="flex gap-1">
+              <Link
+                href="/account"
+                className={buttonVariants({ size: "lg", variant: "secondary" })}
+              >
+                Account
+              </Link>
+              <LogoutButton />
+            </div>
+          )}
+          {user === null && (
+            <Link
+              href="/auth/login"
+              className={buttonVariants({ size: "lg", variant: "default" })}
+            >
+              Sign in
+            </Link>
+          )}
         </nav>
       </div>
     </header>
