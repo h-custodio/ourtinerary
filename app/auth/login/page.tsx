5252c28eaf80753cad99a29675b350f5be3a985c
Frank Mai <frankmai@PowerBook-100.local>
2026-09-03 21:26:32 -0700

    Added styling to the login and signup pages

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,65 +1,85 @@
 "use client";
 
 import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button, buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
+
 import supabase from "@/lib/supabase/client"; // global variable from lib/supabase/client.ts
 
 export default function LoginPage() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [message, setMessage] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
 
-    async function handleSubmit(e: React.SubmitEvent<HTMLFormElement>): Promise<void> {
-        // lets React handle submission
-        // normal browser behaviour is to reload page
-        e.preventDefault();
-        
-        setMessage("Logging in...");
+  async function handleSubmit(
+    e: React.SubmitEvent<HTMLFormElement>,
+  ): Promise<void> {
+    // lets React handle submission
+    // normal browser behaviour is to reload page
+    e.preventDefault();
 
-        // uses Supabase API on the initialized client to log in
-        const { data, error } = await supabase.auth.signInWithPassword({
-            email,
-            password
-        });
+    setMessage("Logging in...");
 
-        // login handling validation
-        if (error) {
-            setMessage(error.message);
-            console.log("error with logging in: ", error.message)
-            return;
-        }
+    // // uses Supabase API on the initialized client to log in
+    // const { data, error } = await supabase.auth.signInWithPassword({
+    //     email,
+    //     password
+    // });
 
-        setMessage("Successfully logged in!");
-        console.log("logged in: ", data.user);
-    }
+    // // login handling validation
+    // if (error) {
+    //     setMessage(error.message);
+    //     console.log("error with logging in: ", error.message)
+    //     return;
+    // }
 
-    return (
-        <div>
-            <h1>Login Page</h1>
+    // setMessage("Successfully logged in!");
+    // console.log("logged in: ", data.user);
+  }
 
-            <form onSubmit={handleSubmit}>
-                <input 
-                type="email" 
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="example@email.com"
-                required // browser validates for empty input 
-                />
-                <br></br>
-                <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="password"  
-                required // browser validates for empty input           
-                />
+  return (
+    <div className="flex-1 max-w-[20%] my-30 mx-auto rounded-2xl border border-border bg-card p-4 shadow-2xl overflow-hidden">
+      <h1 className="mb-4">Login</h1>
 
-                <p>{message}</p>
-                <button>Log In</button>
-                <br></br>
-                <Link href="auth/signup">Sign Up</Link>
-            </form>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-2">
+          <Input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="example@email.com"
+            required // browser validates for empty input
+          />
         </div>
-    );
-}
 
+        <div className="mb-2">
+          <Input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="password"
+            required // browser validates for empty input
+          />
+        </div>
+
+        <p>{message}</p>
+        <div className="flex justify-center mb-2">
+          <Button type="submit">Log In</Button>
+        </div>
+
+        <div className="flex justify-center">
+          <p className="inline-flex items-center gap-1 text-muted-foreground">
+            Don't have an account?
+            <Link
+              href="/auth/signup"
+              className={`${buttonVariants({ size: "default", variant: "link" })} !text-accent !p-0`}
+            >
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+}
