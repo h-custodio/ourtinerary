22f044a6a980fe237cf3415963a71e373a28a620
Frank Mai <frankmai@PowerBook-100.local>
2026-09-06 01:35:59 -0700

    Removed extra styling options

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -25,20 +25,12 @@ const buttonVariants = cva(
         xs: "h-6 gap-1 rounded-[min(var(--radius-md),10px)] px-2 text-xs in-data-[slot=button-group]:rounded-lg has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 [&_svg:not([class*='size-'])]:size-3",
         sm: "h-7 gap-1 rounded-[min(var(--radius-md),12px)] px-2.5 text-[0.8rem] in-data-[slot=button-group]:rounded-lg has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 [&_svg:not([class*='size-'])]:size-3.5",
         lg: "h-9 gap-1.5 px-2.5 has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2",
-        xl: "h-13.5 gap-2.25 px-3.75 has-data-[icon=inline-end]:pr-3 has-data-[icon=inline-start]:pl-3",
-        "2xl":
-          "h-20 gap-3.5 px-5.5 has-data-[icon=inline-end]:pr-4.5 has-data-[icon=inline-start]:pl-4.5",
-        "3xl":
-          "h-30 gap-5 px-8.5 has-data-[icon=inline-end]:pr-7 has-data-[icon=inline-start]:pl-7",
         icon: "size-8",
         "icon-xs":
           "size-6 rounded-[min(var(--radius-md),10px)] in-data-[slot=button-group]:rounded-lg [&_svg:not([class*='size-'])]:size-3",
         "icon-sm":
           "size-7 rounded-[min(var(--radius-md),12px)] in-data-[slot=button-group]:rounded-lg",
         "icon-lg": "size-9",
-        "icon-xl": "size-10",
-        "icon-2xl": "size-11",
-        "icon-3xl": "size-12",
       },
     },
     defaultVariants: {
